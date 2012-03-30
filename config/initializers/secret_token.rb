da13dc9500dd3b56037d17f353fb437a572d8ad9
Grainne O'Neill <grainne@gmail.com>
2012-03-30 11:32:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gideon::Application.config.secret_token = '757873fb9e7f108ab019fcb8cceb3c8ae9631d74a0f048b3fc14634b1dc11756b97ee8a28b4c16599beafbfc31a3ad5bf80268879af5f5c4e82f0acd1ca129ca'
